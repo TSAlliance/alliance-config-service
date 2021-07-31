dc3d3113b423241f2a115eb51ee6b57bad23d190
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2021-07-31 12:15:41 +0200

    added note

--- src/service-management/service-management.service.ts
+++ src/service-management/service-management.service.ts
@@ -7,6 +7,17 @@ import ServiceRepository from './service.repository';
 export class ServiceManagementService {
     constructor(private serviceRepository: ServiceRepository){}
 
+    // Create new validation package -> @tsalliance/validation
+    // Should work for both node and fe
+
+    // Nest-specific:
+    // Should support dependency injection, to inject in class in nestjs
+    // Should have per request scope, so that every request has its own validator
+
+    // FE-specific:
+    // Validator should have constructor to be called on every validation, so this would replace nest's
+    // per request scope
+
     public createService(service: Service): Promise<Service> {
         return this.serviceRepository.save(service);
     }
